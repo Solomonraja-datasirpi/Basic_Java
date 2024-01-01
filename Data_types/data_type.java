c235c0086cb189eb1f61a6d041b9d8c6c9a0d72d
Solomonraja-datasirpi <solomonraja.t@datasirpi.com>
2024-01-01 17:46:32 +0530

    Commit changes

--- Data_types/data_type.java
+++ Data_types/data_type.java
@@ -3,32 +3,49 @@ package Data_types;
 public class data_type{
     public static void main(String args[])
     {
+
+        /* It consists two types of Datatypes, 1. primitive datatype 2.non-primitive datatype*/
+
         System.out.println("Primitive data type"+"\n");
+
+        /*In primitive datatypes consists 8 types, they are listed below */
+
         System.out.println("1.Integer");
         int a = 10000;
         System.out.println(a+"\n");
+
         System.out.println("2.Float");
         Float b = 10.05f;
         System.out.println(b+"\n");
+
         System.out.println("3.Double");
         Double c = 10.00004d;
         System.out.println(c+"\n");
+
         System.out.println("4.short");
         Short d = 1005;
         System.out.println(d+"\n");
+
         System.out.println("5.byte");
         byte e = 10;
         System.out.println(e+"\n");
+
         System.out.println("6.char");
         char f = 's';
         System.out.println(f+"\n");
+
         System.out.println("7.long");
         long g = 100005;
         System.out.println(g+"\n");
+
         System.out.println("8.boolean");
         boolean h = true;
         System.out.println(h+"\n");
+
         System.out.println("non-primitive datatypes"+"\n");
+
+        /*It consists string,arrays,..etc., */
+
         System.out.println("1.String");
         String i="welcome";
         System.out.println(i +"\n");
