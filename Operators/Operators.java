18e797be6a44886cf192d0625a6d79f41255591b
Solomonraja-datasirpi <solomonraja.t@datasirpi.com>
2024-01-03 10:24:50 +0530

    new folder

---
+++ Operators/Operators.java
@@ -0,0 +1,43 @@
+package Operators;
+
+public class Operators {
+
+    public static void main(String args[]){
+
+        int a = 10;
+        int b = 20;
+
+        //Logical operator
+
+        System.out.println(a==10 && b==20);
+        System.out.println(a ==10 || b==35);
+        System.out.println(!(b==30));
+
+        //arimetic operator
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a+b);
+        System.out.println(a+b);
+        System.out.println(a%b);
+
+        //comparision operator
+
+        System.out.println(a>b);
+        System.out.println(a<b);
+        System.out.println(a == b);
+        System.out.println(a != b);
+
+        //assignment operator
+
+        System.out.println(b = b+10);
+        System.out.println(b +=10);
+        System.out.println(b -=10);
+
+        
+
+    }
+    
+}
