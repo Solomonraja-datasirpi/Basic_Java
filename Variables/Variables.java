20f3cc25d648c580dc7a8ccd3ebb5d12cd79be4f
Solomonraja-datasirpi <solomonraja.t@datasirpi.com>
2024-01-04 15:40:17 +0530

    changes

--- Variables/Variables.java
+++ Variables/Variables.java
@@ -1,10 +1,17 @@
 package Variables;
 
+
 public class Variables {
 
     int a = 10; //Instance variable
 
     static int b = 20; //static variable
+
+
+    // static block
+    static{
+        Variables.b=40;
+    }
 
     public static void main(String[] args) {
 
@@ -14,10 +21,11 @@ public class Variables {
 
         System.out.println(b);
 
-        System.out.println(Variables.b);    //without creating an object, we can call with static variables
+        System.out.println(Variables.b);    //Static method --->without creating an object, we can use static variables 
 
         Variables myuse = new Variables();
         System.out.println(myuse.a);
+        System.out.println();
         
     }
     
