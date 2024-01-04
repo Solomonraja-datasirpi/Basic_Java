80d16ed917116d14f246100b1d88ffed63e65abe
Solomonraja-datasirpi <solomonraja.t@datasirpi.com>
2024-01-04 13:05:46 +0530

    new folder

---
+++ Method/Method.java
@@ -0,0 +1,40 @@
+package Method;
+
+public class Method {
+
+    public static void main(String[] args) {
+
+        //multiple methods can have the same name with different parameters:
+
+        add();
+        add(25);
+        add(2,2);
+        add(2,2,8);
+        
+    }
+
+    // method
+
+    static void add(){
+        int a=5;
+        int b=5;
+        int c=a+b;
+        System.out.println(c);
+    }
+
+    //method overloading
+
+    static void add(int a,int b){
+        int c=a+b;
+        System.out.println(c);
+    }
+    static void add(int a,int b,int c){
+        int d=a+b+c;
+        System.out.println(d);
+    }
+
+    static void add(int a){
+        System.out.println(a);
+    }
+    
+}
